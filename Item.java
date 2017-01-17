40ce9c37c75ab742330be177f94f97a2aed2845d
parthpankaj <parthpankaj39@gmail.com>
2017-01-17 22:36:52 +0530

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,21 @@
+package com.example.pankaj.notepp;
+
+public class Item {
+
+    private int id;
+    private String title;
+
+    public Item(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
